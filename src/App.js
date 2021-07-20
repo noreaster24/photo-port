7a353fb25c0686769b7b53621b7b7e64ef0de8c5
Joe Porrazzo <joepa2624@me.com>
2021-07-20 06:57:14 -0700

    first setup and nav bar

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import About from './components/About';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
